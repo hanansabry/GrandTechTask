cb6d132fdc635bc08b880c7247b72814bb436526
Hanan Sabry <eng.hanan.sabry@gmail.com>
2018-09-24 12:47:09 +0200

    Initial Commit

---
+++ app/src/androidTest/java/com/hanan/mstg/grand/grandtechtask/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.hanan.mstg.grand.grandtechtask;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.hanan.mstg.grand.grandtechtask", appContext.getPackageName());
+    }
+}
